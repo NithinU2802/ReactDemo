e021cdad63b025082c364f4c3ecd8ec9fc1ef669
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-07-27 12:11:15 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Learning React</h1>
+      <h2>Everything is around Javascript!</h2>
     </div>
   );
 }
